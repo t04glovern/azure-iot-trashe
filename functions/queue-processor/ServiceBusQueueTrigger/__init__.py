6f10912189cce19c3845459cdd2492609e101bbd
Nathan Glover <nathan@glovers.id.au>
2019-08-31 16:23:04 +0800

    added cosmodb put

--- functions/queue-processor/ServiceBusQueueTrigger/__init__.py
+++ functions/queue-processor/ServiceBusQueueTrigger/__init__.py
@@ -1,7 +1,19 @@
 import azure.functions as func
-
+import azure.cosmos.cosmos_client as cosmos_client
 import logging
 import json
+
+config = {
+    'ENDPOINT': 'https://trashe.documents.azure.com',
+    'PRIMARYKEY': 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    'DATABASE': 'TrasheList',
+    'CONTAINER': 'Items'
+}
+
+# Initialize the Cosmos client
+client = cosmos_client.CosmosClient(url_connection=config['ENDPOINT'], auth={
+                                    'masterKey': config['PRIMARYKEY']})
+
 
 def main(msg: func.ServiceBusMessage):
     result = msg.get_body().decode('utf-8')
@@ -18,3 +30,6 @@ def main(msg: func.ServiceBusMessage):
     #     "iteration": "",
     #     "id": ""
     # }
+
+    # Create and add some items to the container
+    item = client.CreateItem(config['CONTAINER'], result)
